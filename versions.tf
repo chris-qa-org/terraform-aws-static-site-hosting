03df1993253a861622dcb9c7cd50cddab9510aca
Stretch <chris@dxw.com>
2023-04-21 22:34:05 +0100

    Add useast1 required provider
    * Fixes the warning 'There is no explicit declaration for local provider name "aws.useast1"'

--- versions.tf
+++ versions.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 1.4.4"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.63.0"
+      source                = "hashicorp/aws"
+      version               = ">= 4.63.0"
+      configuration_aliases = [aws.useast1]
     }
   }
 }
